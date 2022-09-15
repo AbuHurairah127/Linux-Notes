2a1c7e11043001c8ffd3bdbeb798fadd741779ff
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-09-15 19:12:25 +0500

    file system commands completed

--- commandsUses.js
+++ commandsUses.js
@@ -17,4 +17,21 @@ const LinuxCommands = [
       "cd FolderName (Move to  the directory which name is written if the directory is present in the specific directory in which you are working)",
     useCase5: "cd path (Move to the specific path if the path is true)",
   },
+  {
+    command: "mkdir DirName",
+    use: "Make Directory",
+    useCase1: "mkdir path (To make directory to a specific directory)",
+  },
+  {
+    command: "touch name.txt",
+    use: "To make a new text file",
+  },
+  {
+    command: "mv fileName Path",
+    use: "To move a file to a specific directory",
+  },
+  {
+    command: "cp filename/filepath path",
+    use: "To copy file to specific directory",
+  },
 ];
